7fb672a6082ed488810e757280608874a92864e2
Sean M Higgins <seanmhiggins@SH-MacBook-Pro.local>
2020-12-19 20:40:09 -0600

    second try to netlify form

--- src/components/contact-form.js
+++ src/components/contact-form.js
@@ -1,17 +1,34 @@
 import React from "react"
 
 const ContactForm = () => {
-    return <form name="contact" netlify>
-        <p>
-            <label>Name <input type="text" name="name" /></label>
-        </p>
-        <p>
-            <label>Email <input type="email" name="email" /></label>
-        </p>
-        <p>
-            <button type="submit">Send</button>
-        </p>
+    return <form name="Contact Form" method="POST" data-netlify="true">
+      <input type="hidden" name="form-name" value="Contact Form" />
+      <div>
+        <label>Your Email:</label>
+        <input type="email" name="email" />
+      </div>
+      <div>
+        <label>Message:</label>
+        <textarea name="message" />
+      </div>
+      <button type="submit">Send</button>
     </form>
+    // <form name="contact" netlify netlify-honeypot="bot-field" hidden>
+    //   <input type="text" name="name" />
+    //   <input type="email" name="email" />
+    //   <textarea name="message"></textarea>
+    // </form>
+    // <form name="contact" netlify>
+    //     <p>
+    //         <label>Name <input type="text" name="name" /></label>
+    //     </p>
+    //     <p>
+    //         <label>Email <input type="email" name="email" /></label>
+    //     </p>
+    //     <p>
+    //         <button type="submit">Send</button>
+    //     </p>
+    // </form>
 }
 
 export default ContactForm
